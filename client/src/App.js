21a44ada3428e804f8f1498d59ee489be427bfd1
Nabila Taufiq <nabila.taufiq@verizon.com>
2019-09-15 10:14:23 -0500

    axious and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
-import React, { Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import axios from 'axios';
+// import logo from './logo.svg';
+// import './App.css';
+// import axios from 'axios';
 
+
+class App extends Component {
+  state = {
+    courses: []
+  }
+
+  componentWillMount(){
+    axios.get(`http://localhost:5000/api/courses`).then((response)=> {
+      this.setState({
+        courses:response.data
+      })
+    })
+  }
+  render() {
+    let courses = this.state.courses.map((course)=>{
+      return (
+        <div>
+          <h1>{courses.title}</h1>
+        </div>
+      );
+    });
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+   
+  }
+}
 
 // function App() {
 //   return (
@@ -25,26 +55,26 @@ import axios from 'axios';
 //   );
 // }
 
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      courses : [],
-    };
-  }
+// class App extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       courses : [],
+//     };
+//   }
 
-  axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-  .then(response=>{
-    console.log(response);
-    this.setState({
-      photos: response.data.photos.photo,
-      loading: false
-    });
-  })
-    .catch(error => {
-      console.log('error fetching and parsing data', error);
-    });
+//   axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+//   .then(response=>{
+//     console.log(response);
+//     this.setState({
+//       photos: response.data.photos.photo,
+//       loading: false
+//     });
+//   })
+//     .catch(error => {
+//       console.log('error fetching and parsing data', error);
+//     });
 
-}
+// }
 
 export default App;
